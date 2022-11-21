3fc47a2b7e927d3b808d8850966ad28ef50dbfc0
zay <damilolaoyeniyi13@gmail.com>
2022-11-21 14:26:52 +0100

    bug fix

--- components/Addresss.js
+++ components/Addresss.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box, Menu, MenuItem, MenuButton, MenuList } from '@chakra-ui/react'
-import React from 'react'
 import { useColorModeValue } from '@chakra-ui/react'
 
 const Addresss = () => {
